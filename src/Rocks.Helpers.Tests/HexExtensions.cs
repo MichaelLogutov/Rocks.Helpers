513b746f20f4a27c6e39ba55d451fb831b72db9b
Michael Logutov <logutov@gmail.com>
2014-06-30 14:43:14 +0400

    Added HexExtensions.ToHexString and HexExtensions.HexStringToByteArray

---
+++ src/Rocks.Helpers.Tests/HexExtensions.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+// ReSharper disable ExpressionIsAlwaysNull
+
+namespace Rocks.Helpers.Tests
+{
+	[TestClass]
+	public class HexExtensions
+	{
+		[TestMethod]
+		public void ToHexString_EmptyArray_ReturnsEmptyString ()
+		{
+			// arrange
+			var data = new byte[0];
+
+
+			// act
+			var result = data.ToHexString ();
+
+
+			// assert
+			result.Should ().BeEmpty ();
+		}
+
+
+		[TestMethod]
+		public void ToHexString_Null_ReturnsEmptyString ()
+		{
+			// arrange
+			byte[] data = null;
+
+
+			// act
+			var result = data.ToHexString ();
+
+
+			// assert
+			result.Should ().BeEmpty ();
+		}
+
+
+		[TestMethod]
+		public void ToHexString_ReturnsCorrectString ()
+		{
+			// arrange
+			var data = new byte[] { 1, 10, 3 };
+
+
+			// act
+			var result = data.ToHexString ();
+
+
+			// assert
+			result.Should ().Be ("010A03");
+		}
+
+
+		[TestMethod]
+		public void HexStringToByteArray_EmptyString_ReturnsEmptyArray ()
+		{
+			// arrange
+			var data = string.Empty;
+
+
+			// act
+			var result = data.HexStringToByteArray ();
+
+
+			// assert
+			result.Should ().BeEmpty ();
+		}
+
+
+		[TestMethod]
+		public void HexStringToByteArray_Null_ReturnsEmptyArray ()
+		{
+			// arrange
+			string data = null;
+
+
+			// act
+			var result = data.HexStringToByteArray ();
+
+
+			// assert
+			result.Should ().BeEmpty ();
+		}
+
+
+		[TestMethod]
+		public void HexStringToByteArray_ReturnsCorrectArray ()
+		{
+			// arrange
+			var data = "010A03";
+
+
+			// act
+			var result = data.HexStringToByteArray ();
+
+
+			// assert
+			result.Should ().Equal (new byte[] { 1, 10, 3 });
+		}
+	}
+}
---
+++ src/Rocks.Helpers/HexExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using JetBrains.Annotations;
+
+namespace Rocks.Helpers
+{
+	public static class HexExtensions
+	{
+		/// <summary>
+		/// Converts <paramref name="bytes"/> to string using hex (upper case) representation.
+		/// </summary>
+		/// <param name="bytes">Source bytes. If null or empty returns empty string.</param>
+		[NotNull]
+		public static string ToHexString ([CanBeNull] this IList<byte> bytes)
+		{
+			if (bytes == null || bytes.Count == 0)
+				return string.Empty;
+
+			var c = new char[bytes.Count * 2];
+
+			for (var i = 0; i < bytes.Count; i++)
+			{
+				var b = bytes[i] >> 4;
+				c[i * 2] = (char) (55 + b + (((b - 10) >> 31) & -7));
+				b = bytes[i] & 0xF;
+				c[i * 2 + 1] = (char) (55 + b + (((b - 10) >> 31) & -7));
+			}
+
+			return new string (c);
+		}
+
+
+		/// <summary>
+		/// Converts source hex string to byte array.
+		/// </summary>
+		/// <param name="str">Source string. If null or empty returns empty array.</param>
+		[NotNull]
+		public static byte[] HexStringToByteArray ([CanBeNull] this string str)
+		{
+			if (string.IsNullOrEmpty (str))
+				return new byte[0];
+
+			if (str.Length % 2 == 1)
+				str = '0' + str;
+
+			var buffer = new byte[str.Length / 2];
+
+			for (var i = 0; i < buffer.Length; ++i)
+				buffer[i] = byte.Parse (str.Substring (i * 2, 2), NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+
+			return buffer;
+		}
+	}
+}
