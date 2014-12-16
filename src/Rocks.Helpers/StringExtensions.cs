ca2bedc72eea8a3e5f6f42ae15f6f9698bcc90bd
Michael Logutov <logutov@gmail.com>
2014-12-16 11:54:32 +0300

    split to longs

--- src/Rocks.Helpers/StringExtensions.cs
+++ src/Rocks.Helpers/StringExtensions.cs
@@ -8,9 +8,9 @@ namespace Rocks.Helpers
 	public static class StringExtensions
 	{
 		/// <summary>
-		/// Trims the source string to specified length if it exceeds it.
-		/// Optionally adds <paramref name="prefix"/> to the end of the string if it was truncated.
-		/// If source string is less that <paramref name="maxLength"/> then the source string returned.
+		///     Trims the source string to specified length if it exceeds it.
+		///     Optionally adds <paramref name="prefix" /> to the end of the string if it was truncated.
+		///     If source string is less that <paramref name="maxLength" /> then the source string returned.
 		/// </summary>
 		/// <param name="str">Source string (can be null).</param>
 		/// <param name="maxLength">Maximum length.</param>
@@ -30,7 +30,7 @@ namespace Rocks.Helpers
 
 
 		/// <summary>
-		/// Compares two string using <see cref="StringComparison.OrdinalIgnoreCase"/>.
+		///     Compares two string using <see cref="StringComparison.OrdinalIgnoreCase" />.
 		/// </summary>
 		/// <param name="strA">String A (can be null).</param>
 		/// <param name="strB">Stirng B (can be null).</param>
@@ -40,11 +40,14 @@ namespace Rocks.Helpers
 		}
 
 
+		/// <summary>
+		///     Splits the source string using specified separator. If source string is null the returns new empty list.
+		/// </summary>
 		[ContractAnnotation ("values:null => stop")]
 		public static IList<string> SplitNullSafe (this string values, params char[] separator)
 		{
 			if (values == null)
-				return new string[0];
+				return new List<string> ();
 
 			if (separator.IsNullOrEmpty ())
 				separator = new[] { ',' };
@@ -54,8 +57,8 @@ namespace Rocks.Helpers
 
 
 		/// <summary>
-		/// Splits source <paramref name="str"/> by specified <paramref name="separators"/>
-		/// into a list or trimmed, non null or empty items.
+		///     Splits source <paramref name="str" /> by specified <paramref name="separators" />
+		///     into a list or trimmed, non null or empty items.
 		/// </summary>
 		/// <param name="str">Source string (can be null).</param>
 		/// <param name="separators">Separators list.</param>
@@ -66,11 +69,27 @@ namespace Rocks.Helpers
 		}
 
 
+		/// <summary>
+		///     Splits the string into ints, ignoring all values that can not be converted to number.
+		/// </summary>
 		[ContractAnnotation ("values:null => stop")]
-		public static IList<int> SplitToInts (this string values, params char[] separator)
+		public static IList<int> SplitToInts (this string values, params char[] separators)
 		{
-			return (from x in values.SplitToTrimmedNonEmptyStringList ()
+			return (from x in values.SplitToTrimmedNonEmptyStringList (separators)
 			        let id = x.ToInt ()
+			        where id != null
+			        select id.Value).ToList ();
+		}
+
+
+		/// <summary>
+		///     Splits the string into ints, ignoring all values that can not be converted to number.
+		/// </summary>
+		[ContractAnnotation ("values:null => stop")]
+		public static IList<long> SplitToLongs (this string values, params char[] separator)
+		{
+			return (from x in values.SplitToTrimmedNonEmptyStringList (separator)
+			        let id = x.ToLong ()
 			        where id != null
 			        select id.Value).ToList ();
 		}
