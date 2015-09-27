ffafe90f5b4ca74bda2bf741b95c504b558a3096
Michael Logutov <logutov@gmail.com>
2015-09-27 22:40:45 +0300

    fixed bug with fastmember static fields

--- src/Rocks.Helpers.Tests/UrlExtensionsTests.cs
+++ src/Rocks.Helpers.Tests/UrlExtensionsTests.cs
@@ -6,253 +6,282 @@ using Xunit;
 
 namespace Rocks.Helpers.Tests
 {
-	public class UrlExtensionsTests
-	{
-		[Fact]
-		public void PropertiesToRouteValueDictionary_Null_ReturnsEmpty ()
-		{
-			var result = ((object) null).PropertiesToRouteValueDictionary ();
+    public class UrlExtensionsTests
+    {
+        [Fact]
+        public void PropertiesToRouteValueDictionary_Null_ReturnsEmpty ()
+        {
+            var result = ((object) null).PropertiesToRouteValueDictionary ();
 
-			result.Should ().BeEmpty ();
-		}
+            result.Should ().BeEmpty ();
+        }
 
 
-		[Fact]
-		public void PropertiesToRouteValueDictionary_NotNull_ReturnsCorrectList ()
-		{
-			var result = new { a = "b" }.PropertiesToRouteValueDictionary ();
+        [Fact]
+        public void PropertiesToRouteValueDictionary_NotNull_ReturnsCorrectList ()
+        {
+            var result = new { a = "b" }.PropertiesToRouteValueDictionary ();
 
-			result.Should ().Equal (new Dictionary<string, object>
-			{
-				{ "a", "b" }
-			});
-		}
+            result.Should ().Equal (new Dictionary<string, object>
+                                    {
+                                        { "a", "b" }
+                                    });
+        }
 
 
-		private class DisplayFormatAttributeTestObject : IExplicitInterface
-		{
-			[DisplayFormat (DataFormatString = "{0:dd MM yyyy}")]
-			public DateTime Date { get; set; }
+        private class DisplayFormatAttributeTestObject : IExplicitInterface
+        {
+            [DisplayFormat (DataFormatString = "{0:dd MM yyyy}")]
+            public DateTime Date { get; set; }
 
 
-			[DisplayFormat (DataFormatString = "{0:#.00}")]
-			public decimal Number { get; set; }
+            [DisplayFormat (DataFormatString = "{0:#.00}")]
+            public decimal Number { get; set; }
 
-			[DisplayFormat (HtmlEncode = true, NullDisplayText = "null")]
-			public string HtmlEncodedString { get; set; }
+            [DisplayFormat (HtmlEncode = true, NullDisplayText = "null")]
+            public string HtmlEncodedString { get; set; }
 
-			string IExplicitInterface.ExplicitProperty { get; set; }
-		}
+            string IExplicitInterface.ExplicitProperty { get; set; }
+        }
 
 
-		private interface IExplicitInterface
-		{
-			string ExplicitProperty { get; set; }
-		}
+        private interface IExplicitInterface
+        {
+            string ExplicitProperty { get; set; }
+        }
 
 
-		[Fact]
-		public void PropertiesToRouteValueDictionary_PropertyWithDisplayFormatAttribute_UsesDataFormatString ()
-		{
-			// arrange
-			var obj = new DisplayFormatAttributeTestObject
-			{
-				Date = new DateTime (2000, 1, 2),
-				Number = 1.234m
-			};
+        [Fact]
+        public void PropertiesToRouteValueDictionary_PropertyWithDisplayFormatAttribute_UsesDataFormatString ()
+        {
+            // arrange
+            var obj = new DisplayFormatAttributeTestObject
+                      {
+                          Date = new DateTime (2000, 1, 2),
+                          Number = 1.234m
+                      };
 
 
-			// act
-			var result = obj.PropertiesToRouteValueDictionary ();
+            // act
+            var result = obj.PropertiesToRouteValueDictionary ();
 
 
-			// assert
-			result.ShouldAllBeEquivalentTo (new[]
-			{
-				new KeyValuePair<string, object> ("Date", "02 01 2000"),
-				new KeyValuePair<string, object> ("Number", "1.23"),
-				new KeyValuePair<string, object> ("HtmlEncodedString", "null")
-			});
-		}
+            // assert
+            result.ShouldAllBeEquivalentTo (new[]
+                                            {
+                                                new KeyValuePair<string, object> ("Date", "02 01 2000"),
+                                                new KeyValuePair<string, object> ("Number", "1.23"),
+                                                new KeyValuePair<string, object> ("HtmlEncodedString", "null")
+                                            });
+        }
 
 
-		[Fact]
-		public void PropertiesToRouteValueDictionary_PropertyWithDisplayFormatAttribute_AndHtmlEncodeTrue_ShouldNotHtmlEncode ()
-		{
-			// arrange
-			var obj = new DisplayFormatAttributeTestObject
-			{
-				HtmlEncodedString = "a b"
-			};
+        [Fact]
+        public void PropertiesToRouteValueDictionary_PropertyWithDisplayFormatAttribute_AndHtmlEncodeTrue_ShouldNotHtmlEncode ()
+        {
+            // arrange
+            var obj = new DisplayFormatAttributeTestObject
+                      {
+                          HtmlEncodedString = "a b"
+                      };
 
 
-			// act
-			var result = obj.PropertiesToRouteValueDictionary ();
+            // act
+            var result = obj.PropertiesToRouteValueDictionary ();
 
 
-			// assert
-			result.Should ().Contain ("HtmlEncodedString", "a b");
-		}
+            // assert
+            result.Should ().Contain ("HtmlEncodedString", "a b");
+        }
 
 
-		[Fact]
-		public void PropertiesToRouteValueDictionary_ExplicitPropertySet_IgnoresExplicitProperty ()
-		{
-			// arrange
-			var obj = new DisplayFormatAttributeTestObject ();
-			((IExplicitInterface) obj).ExplicitProperty = "test";
+        [Fact]
+        public void PropertiesToRouteValueDictionary_ExplicitPropertySet_IgnoresExplicitProperty ()
+        {
+            // arrange
+            var obj = new DisplayFormatAttributeTestObject ();
+            ((IExplicitInterface) obj).ExplicitProperty = "test";
 
 
-			// act
-			var result = obj.PropertiesToRouteValueDictionary ();
+            // act
+            var result = obj.PropertiesToRouteValueDictionary ();
 
 
-			// assert
-			result.Should ().NotContain ("ExplicitProperty", "test");
-		}
+            // assert
+            result.Should ().NotContain ("ExplicitProperty", "test");
+        }
 
 
-		[Fact]
-		public void PropertiesToQueryParameters_Null_ReturnsEmpty ()
-		{
-			// arrange
+        [Fact]
+        public void PropertiesToQueryParameters_Null_ReturnsEmpty ()
+        {
+            // arrange
 
 
-			// act
-			var result = ((object) null).PropertiesToQueryParameters ();
+            // act
+            var result = ((object) null).PropertiesToQueryParameters ();
 
 
-			// assert
-			result.Should ().BeEmpty ();
-		}
+            // assert
+            result.Should ().BeEmpty ();
+        }
 
 
-		[Fact]
-		public void ToQueryStringParameters_Null_ReturnsEmpty ()
-		{
-			// arrange
+        [Fact]
+        public void ToQueryStringParameters_Null_ReturnsEmpty ()
+        {
+            // arrange
 
 
-			// act
-			var result = ((IEnumerable<KeyValuePair<string, object>>) null).ToQueryStringParameters ();
+            // act
+            var result = ((IEnumerable<KeyValuePair<string, object>>) null).ToQueryStringParameters ();
 
 
-			// assert
-			result.Should ().BeEmpty ();
-		}
+            // assert
+            result.Should ().BeEmpty ();
+        }
 
 
-		[Fact]
-		public void ToQueryStringParameters_Empty_ReturnsEmpty ()
-		{
-			// arrange
-			var data = new Dictionary<string, object> ();
+        [Fact]
+        public void ToQueryStringParameters_Empty_ReturnsEmpty ()
+        {
+            // arrange
 
 
-			// act
-			var result = data.ToQueryStringParameters ();
+            // act
+            var result = new Dictionary<string, object> ().ToQueryStringParameters ();
 
 
-			// assert
-			result.Should ().BeEmpty ();
-		}
+            // assert
+            result.Should ().BeEmpty ();
+        }
 
 
-		[Fact]
-		public void ToQueryStringParameters_OneItem_ReturnsCorrectQueryString ()
-		{
-			// arrange
-			var data = new Dictionary<string, object>
-			{
-				{ "a", 1 }
-			};
+        [Fact]
+        public void ToQueryStringParameters_OneItem_ReturnsCorrectQueryString ()
+        {
+            // arrange
+            var data = new Dictionary<string, object>
+                       {
+                           { "a", 1 }
+                       };
 
 
-			// act
-			var result = data.ToQueryStringParameters ();
+            // act
+            var result = data.ToQueryStringParameters ();
 
 
-			// assert
-			result.Should ().Be ("?a=1");
-		}
+            // assert
+            result.Should ().Be ("?a=1");
+        }
 
 
-		[Fact]
-		public void ToQueryStringParameters_TwoItems_UsesAmpersand ()
-		{
-			// arrange
-			var data = new List<KeyValuePair<string, object>> 
-			{
-				new KeyValuePair<string, object> ("a", 1),
-				new KeyValuePair<string, object> ("b", 2)
-			};
+        [Fact]
+        public void ToQueryStringParameters_TwoItems_UsesAmpersand ()
+        {
+            // arrange
+            var data = new List<KeyValuePair<string, object>>
+                       {
+                           new KeyValuePair<string, object> ("a", 1),
+                           new KeyValuePair<string, object> ("b", 2)
+                       };
 
 
-			// act
-			var result = data.ToQueryStringParameters ();
+            // act
+            var result = data.ToQueryStringParameters ();
 
 
-			// assert
-			result.Should ().Be ("?a=1&b=2");
-		}
+            // assert
+            result.Should ().Be ("?a=1&b=2");
+        }
 
 
-		[Fact]
-		public void ToQueryStringParameters_OneItemIsEnumerable_AddsAllItemItems ()
-		{
-			// arrange
-			var data = new Dictionary<string, object>
-			{
-				{ "a", new[] { 1, 2, 3 } }
-			};
+        [Fact]
+        public void ToQueryStringParameters_OneItemIsEnumerable_AddsAllItemItems ()
+        {
+            // arrange
+            var data = new Dictionary<string, object>
+                       {
+                           { "a", new[] { 1, 2, 3 } }
+                       };
 
 
-			// act
-			var result = data.ToQueryStringParameters ();
+            // act
+            var result = data.ToQueryStringParameters ();
 
 
-			// assert
-			result.Should ().Be ("?a=1&a=2&a=3");
-		}
+            // assert
+            result.Should ().Be ("?a=1&a=2&a=3");
+        }
 
 
-		[Fact]
-		public void ToQueryStringParameters_ItemNameCapitalized_LowerCaseOfTheFirstLetterForTheName ()
-		{
-			// arrange
-			var data = new Dictionary<string, object>
-			{
-				{ "AaA", "BbB" }
-			};
+        [Fact]
+        public void ToQueryStringParameters_ItemNameCapitalized_LowerCaseOfTheFirstLetterForTheName ()
+        {
+            // arrange
+            var data = new Dictionary<string, object>
+                       {
+                           { "AaA", "BbB" }
+                       };
 
 
-			// act
-			var result = data.ToQueryStringParameters ();
+            // act
+            var result = data.ToQueryStringParameters ();
 
 
-			// assert
-			result.Should ().Be ("?aaA=BbB");
-		}
+            // assert
+            result.Should ().Be ("?aaA=BbB");
+        }
 
 
-		[Fact]
-		public void ToQueryStringParameters_UsesHtmlEncodeForItemValue ()
-		{
-			// arrange
-			var data = new Dictionary<string, object>
-			{
-				{ "a", "a b c" }
-			};
+        [Fact]
+        public void ToQueryStringParameters_UsesHtmlEncodeForItemValue ()
+        {
+            // arrange
+            var data = new Dictionary<string, object>
+                       {
+                           { "a", "a b c" }
+                       };
 
 
-			// act
-			var result = data.ToQueryStringParameters ();
+            // act
+            var result = data.ToQueryStringParameters ();
 
 
-			// assert
-			result.Should ().Be ("?a=a%20b%20c");
-		}
-	}
+            // assert
+            result.Should ().Be ("?a=a%20b%20c");
+        }
+
+
+        private class Data
+        {
+            // ReSharper disable once UnusedMember.Local
+            public const string Const = "a";
+
+#pragma warning disable 169
+            public static string Static = "b";
+#pragma warning restore 169
+
+            public string String { get; set; }
+        }
+
+
+        [Fact]
+        public void PropertiesToRouteValueDictionary_ClassWithStaticMembers_ReturnOnlyNonStatic ()
+        {
+            // arrange
+            var data = new Data { String = "abc" };
+
+
+            // act
+            var result = data.PropertiesToRouteValueDictionary ();
+
+
+            // assert
+            result.Should ().Equal (new Dictionary<string, object>
+                                    {
+                                        { "String", "abc" }
+                                    });
+        }
+    }
 }
-
-
--- src/Rocks.Helpers/UrlExtensions.cs
+++ src/Rocks.Helpers/UrlExtensions.cs
@@ -14,202 +14,205 @@ using JetBrains.Annotations;
 
 namespace Rocks.Helpers
 {
-	public static class UrlExtensions
-	{
-		#region Private fields
+    public static class UrlExtensions
+    {
+        #region Private fields
 
-		private static readonly ConcurrentDictionary<string, TypeInfo> typesInfoCache = new ConcurrentDictionary<string, TypeInfo> (StringComparer.Ordinal);
+        private static readonly ConcurrentDictionary<string, TypeInfo> typesInfoCache =
+            new ConcurrentDictionary<string, TypeInfo> (StringComparer.Ordinal);
 
-		#endregion
+        #endregion
 
-		#region Static methods
+        #region Static methods
 
-		/// <summary>
-		///     Creates new <see cref="RouteValueDictionary" /> from <paramref name="obj" /> properties.
-		///     Optionally filters property names by <paramref name="filter" />.
-		/// </summary>
-		/// <param name="obj">Object which properties will be added to <see cref="RouteValueDictionary" />. Can be null.</param>
-		/// <param name="filter">
-		///     Optional filter function that should return true for every property and it's current value
-		///     that should be included in the result.
-		/// </param>
-		[NotNull]
-		public static RouteValueDictionary PropertiesToRouteValueDictionary ([CanBeNull] this object obj, Func<Member, object, bool> filter = null)
-		{
-			var res = new RouteValueDictionary ();
+        /// <summary>
+        ///     Creates new <see cref="RouteValueDictionary" /> from <paramref name="obj" /> properties.
+        ///     Optionally filters property names by <paramref name="filter" />.
+        /// </summary>
+        /// <param name="obj">Object which properties will be added to <see cref="RouteValueDictionary" />. Can be null.</param>
+        /// <param name="filter">
+        ///     Optional filter function that should return true for every property that should be included in the result.
+        /// </param>
+        [NotNull]
+        public static RouteValueDictionary PropertiesToRouteValueDictionary ([CanBeNull] this object obj, Func<PropertyInfo, bool> filter = null)
+        {
+            var res = new RouteValueDictionary ();
 
-			if (obj == null)
-				return res;
+            if (obj == null)
+                return res;
 
-			var object_type = obj.GetType ();
-			var type_accessor = TypeAccessor.Create (object_type);
-			var type_info = GetTypeInfo (object_type);
+            var object_type = obj.GetType ();
+            var type_accessor = TypeAccessor.Create (object_type);
+            var type_info = GetTypeInfo (object_type);
 
-			foreach (var member in type_accessor.GetMembers ())
-			{
-                var value = type_accessor[obj, member.Name];
-
-				if (filter != null && !filter (member, value))
-					continue;
-
-				DisplayFormatAttribute display_format_attribute;
-				if (type_info.PropertiesCustomFormat.TryGetValue (member.Name, out display_format_attribute))
-					value = FormatValue (value, display_format_attribute);
-
-				res.Add (member.Name, value);
-			}
-
-			return res;
-		}
-
-
-		/// <summary>
-		///     Creates HTTP GET query parameters string from the supplied <paramref name="obj" /> properties.
-		///     Optionally filters property names by <paramref name="filter" />.
-		///     Adds specified <paramref name="prefix" /> to the start of the result string if there are any
-		///     parameters in the resulting query string.
-		/// </summary>
-		/// <param name="obj">Object which properties will be the result query string. Can be null.</param>
-		/// <param name="prefix">
-		///     Prefix that will be added to the to the start of the result string if there are any
-		///     parameters in the resulting query string.
-		/// </param>
-		/// <param name="filter">
-		///     Optional filter function that should return true for every property and it's current value
-		///     that should be included in the result.
-		/// </param>
-		public static string PropertiesToQueryParameters (this object obj, string prefix = "?", Func<Member, object, bool> filter = null)
-		{
-			if (obj == null)
-				return string.Empty;
-
-			return obj.PropertiesToRouteValueDictionary (filter).ToQueryStringParameters (prefix);
-		}
-
-
-		/// <summary>
-		///     Creates HTTP GET query parameters string from the supplied <paramref name="values" />.
-		///     Adds specified <paramref name="prefix" /> to the start of the result string if there are any
-		///     parameters in the resulting query string.
-		/// </summary>
-		/// <param name="values">A list of values that will be added to the result query string.</param>
-		/// <param name="prefix">
-		///     Prefix that will be added to the to the start of the result string if there are any
-		///     parameters in the resulting query string.
-		/// </param>
-		[SuppressMessage ("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures")]
-		public static string ToQueryStringParameters (this IEnumerable<KeyValuePair<string, object>> values, string prefix = "?")
-		{
-			if (values == null)
-				return string.Empty;
-
-			var result = new StringBuilder (250);
-
-			foreach (var kv in values.Where (x => x.Value != null))
-			{
-				if (kv.Value is string)
-				{
-					AppendQueryStringValue (result, kv.Key, kv.Value);
-					continue;
-				}
-
-				var e = kv.Value as IEnumerable;
-				if (e != null)
-				{
-					var enm = e.GetEnumerator ();
-					while (enm.MoveNext ())
-						AppendQueryStringValue (result, kv.Key, enm.Current);
-
-					continue;
-				}
-
-				AppendQueryStringValue (result, kv.Key, kv.Value);
-			}
-
-			if (result.Length == 0)
-				return string.Empty;
-
-			return prefix + result;
-		}
-
-		#endregion
-
-		#region Private methods
-
-		private static object FormatValue (object value, [NotNull] DisplayFormatAttribute displayFormatAttribute)
-		{
-			if (value == null)
-				return displayFormatAttribute.NullDisplayText;
-
-			if (string.IsNullOrEmpty (displayFormatAttribute.DataFormatString))
-				return value;
-
-			return string.Format (CultureInfo.InvariantCulture, displayFormatAttribute.DataFormatString, value);
-		}
-
-
-		private static TypeInfo GetTypeInfo (Type type)
-		{
-			TypeInfo type_info;
-			var cache_key = type.FullName;
-
-			if (typesInfoCache.TryGetValue (cache_key, out type_info))
-				return type_info;
-
-			type_info = new TypeInfo
-			{
-				PropertiesCustomFormat = new Dictionary<string, DisplayFormatAttribute> ()
-			};
-
-
-			foreach (var member in TypeAccessor.Create (type).GetMembers ())
-			{
-				var property = type.GetProperty (member.Name);
-                if (property == null)
+            foreach (var member in type_accessor.GetMembers ())
+            {
+                PropertyInfo property;
+                if (!type_info.Properties.TryGetValue (member.Name, out property))
                     continue;
 
-				var attr = property.GetCustomAttribute<DisplayFormatAttribute> (false);
-				if (attr == null)
-					continue;
+                if (filter != null && !filter (property))
+                    continue;
 
-				type_info.PropertiesCustomFormat[property.Name] = attr;
-			}
+                var value = type_accessor[obj, member.Name];
 
-			typesInfoCache.TryAdd (cache_key, type_info);
+                DisplayFormatAttribute display_format_attribute;
+                if (type_info.PropertiesCustomFormat.TryGetValue (member.Name, out display_format_attribute))
+                    value = FormatValue (value, display_format_attribute);
 
-			return type_info;
-		}
+                res.Add (member.Name, value);
+            }
+
+            return res;
+        }
 
 
-		[SuppressMessage ("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
-		private static void AppendQueryStringValue ([NotNull] StringBuilder result, [NotNull] string key, [NotNull] object value, bool escapeValue = true)
-		{
-			var str = value is string ? (string) value : Convert.ToString (value, CultureInfo.InvariantCulture);
-			if (str.Length == 0)
-				return;
+        /// <summary>
+        ///     Creates HTTP GET query parameters string from the supplied <paramref name="obj" /> properties.
+        ///     Optionally filters property names by <paramref name="filter" />.
+        ///     Adds specified <paramref name="prefix" /> to the start of the result string if there are any
+        ///     parameters in the resulting query string.
+        /// </summary>
+        /// <param name="obj">Object which properties will be the result query string. Can be null.</param>
+        /// <param name="prefix">
+        ///     Prefix that will be added to the to the start of the result string if there are any
+        ///     parameters in the resulting query string.
+        /// </param>
+        /// <param name="filter">
+        ///     Optional filter function that should return true for every property that should be included in the result.
+        /// </param>
+        public static string PropertiesToQueryParameters (this object obj, string prefix = "?", Func<PropertyInfo, bool> filter = null)
+        {
+            if (obj == null)
+                return string.Empty;
 
-			if (result.Length > 0)
-				result.Append ('&');
+            return obj.PropertiesToRouteValueDictionary (filter).ToQueryStringParameters (prefix);
+        }
 
-			result.Append (char.ToLower (key[0], CultureInfo.InvariantCulture));
-			result.Append (key, 1, key.Length - 1);
-			result.Append ('=');
-			result.Append (escapeValue ? Uri.EscapeDataString (str) : str);
-		}
 
-		#endregion
+        /// <summary>
+        ///     Creates HTTP GET query parameters string from the supplied <paramref name="values" />.
+        ///     Adds specified <paramref name="prefix" /> to the start of the result string if there are any
+        ///     parameters in the resulting query string.
+        /// </summary>
+        /// <param name="values">A list of values that will be added to the result query string.</param>
+        /// <param name="prefix">
+        ///     Prefix that will be added to the to the start of the result string if there are any
+        ///     parameters in the resulting query string.
+        /// </param>
+        [SuppressMessage ("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures")]
+        public static string ToQueryStringParameters (this IEnumerable<KeyValuePair<string, object>> values, string prefix = "?")
+        {
+            if (values == null)
+                return string.Empty;
 
-		#region Nested type: TypeInfo
+            var result = new StringBuilder (250);
 
-		private class TypeInfo
-		{
-			#region Public properties
+            foreach (var kv in values.Where (x => x.Value != null))
+            {
+                if (kv.Value is string)
+                {
+                    AppendQueryStringValue (result, kv.Key, kv.Value);
+                    continue;
+                }
 
-			public IDictionary<string, DisplayFormatAttribute> PropertiesCustomFormat { get; set; }
+                var e = kv.Value as IEnumerable;
+                if (e != null)
+                {
+                    var enm = e.GetEnumerator ();
+                    while (enm.MoveNext ())
+                        AppendQueryStringValue (result, kv.Key, enm.Current);
 
-			#endregion
-		}
+                    continue;
+                }
 
-		#endregion
-	}
+                AppendQueryStringValue (result, kv.Key, kv.Value);
+            }
+
+            if (result.Length == 0)
+                return string.Empty;
+
+            return prefix + result;
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private static object FormatValue (object value, [NotNull] DisplayFormatAttribute displayFormatAttribute)
+        {
+            if (value == null)
+                return displayFormatAttribute.NullDisplayText;
+
+            if (string.IsNullOrEmpty (displayFormatAttribute.DataFormatString))
+                return value;
+
+            return string.Format (CultureInfo.InvariantCulture, displayFormatAttribute.DataFormatString, value);
+        }
+
+
+        private static TypeInfo GetTypeInfo (Type type)
+        {
+            return typesInfoCache.GetOrAdd
+                (type.FullName,
+                 _ =>
+                 {
+                     var type_info =
+                         new TypeInfo
+                         {
+                             PropertiesCustomFormat = new Dictionary<string, DisplayFormatAttribute> (),
+                             Properties = new Dictionary<string, PropertyInfo> ()
+                         };
+
+                     foreach (var property in type.GetProperties (BindingFlags.Public | BindingFlags.Instance))
+                     {
+                         type_info.Properties[property.Name] = property;
+
+                         var attr = property.GetCustomAttribute<DisplayFormatAttribute> (false);
+                         if (attr == null)
+                             continue;
+
+                         type_info.PropertiesCustomFormat[property.Name] = attr;
+                     }
+
+                     return type_info;
+                 });
+        }
+
+
+        [SuppressMessage ("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
+        private static void AppendQueryStringValue ([NotNull] StringBuilder result,
+                                                    [NotNull] string key,
+                                                    [NotNull] object value,
+                                                    bool escapeValue = true)
+        {
+            var str = value is string ? (string) value : Convert.ToString (value, CultureInfo.InvariantCulture);
+            if (str.Length == 0)
+                return;
+
+            if (result.Length > 0)
+                result.Append ('&');
+
+            result.Append (char.ToLower (key[0], CultureInfo.InvariantCulture));
+            result.Append (key, 1, key.Length - 1);
+            result.Append ('=');
+            result.Append (escapeValue ? Uri.EscapeDataString (str) : str);
+        }
+
+        #endregion
+
+        #region Nested type: TypeInfo
+
+        private class TypeInfo
+        {
+            #region Public properties
+
+            public IDictionary<string, DisplayFormatAttribute> PropertiesCustomFormat { get; set; }
+            public IDictionary<string, PropertyInfo> Properties { get; set; }
+
+            #endregion
+        }
+
+        #endregion
+    }
 }
