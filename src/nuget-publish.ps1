108f006e19a8a19edf8306e5a8160d6793c4c819
Michael Logutov <michaellogutov@live.com>
2016-09-13 20:33:04 +0300

    Version bump

--- src/nuget-publish.ps1
+++ src/nuget-publish.ps1
@@ -8,6 +8,6 @@ iex "nuget pack -build -Prop Configuration=Release -sym $id\$id.csproj"
 $package_file = @(Get-ChildItem "*.nupkg" -Exclude "*.symbols.*" | Sort-Object -Property CreationTime -Descending)[0]
 $package_file.Name
 
-iex ("nuget push " + $package_file.Name)
+iex ("nuget push " + $package_file.Name + " -source nuget.org")
 
 Remove-Item *.nupkg
