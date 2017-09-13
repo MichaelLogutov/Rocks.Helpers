bb60138e3cdd376468ac23db7a60eacf6daa089b
Pavel Kurkin <p.kurkin@cian.ru>
2017-09-13 14:40:13 +0300

    Update year in license, DbFactory initialize DbProviderFactory from config and built in classes

--- src/Rocks.Helpers/DbFactory.cs
+++ src/Rocks.Helpers/DbFactory.cs
@@ -1,44 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Data.Common;
+using System.Data.SqlClient;
+using System.Reflection;
 
 
 namespace Rocks.Helpers
 {
     public static class DbFactory
     {
-        private static readonly Dictionary<string, Type> m_Descriptions = new Dictionary<string, Type>();
-        
-        public static Func<Type, DbProviderFactory> CreateInstance
-        {
-            private get;
-            set;
-        }
+        private static bool m_IsInitialized = false;
 
-        public static void Set<TFactory>(string providerName)
-            where TFactory : DbProviderFactory
-        {
-            var factoryType = typeof (TFactory);
+        private static readonly Dictionary<string, DbProviderFactory> m_Descriptions = new Dictionary<string, DbProviderFactory>();
 
+        public static Func<DbProviderFactory, DbProviderFactory> CreateInstance { private get; set; }
+
+        public static void Set<T>(string providerName, T providerInstance) where T : DbProviderFactory
+        {
             lock (m_Descriptions)
             {
                 if (m_Descriptions.ContainsKey(providerName))
                 {
-                    m_Descriptions[providerName] = factoryType;
+                    m_Descriptions[providerName] = providerInstance;
                 }
                 else
                 {
-                    m_Descriptions.Add(providerName,
-                                       factoryType);
+                    m_Descriptions.Add(providerName, providerInstance);
                 }
             }
         }
 
         public static DbProviderFactory Get(string providerName)
         {
+            EnsureIsInitialized();
+
             lock (m_Descriptions)
             {
-                if (m_Descriptions.ContainsKey(providerName))
+                if (m_Descriptions.ContainsKey(providerName.ToLower()))
                 {
                     return ConstructInstance(m_Descriptions[providerName]);
                 }
@@ -49,23 +49,89 @@ namespace Rocks.Helpers
 
         public static DbProviderFactory Get(DbConnection dbConnection)
         {
-            var providerName = dbConnection.GetType()
-                                           .FullName;
-
-            lock (m_Descriptions)
-            {
-                if (m_Descriptions.ContainsKey(providerName))
-                {
-                    return ConstructInstance(m_Descriptions[providerName]);
-                }
-            }
-
-            return null;
+            var providerName = dbConnection.GetType().Namespace;
+            return Get(providerName);
         }
 
-        private static DbProviderFactory ConstructInstance(Type type)
+        private static DbProviderFactory ConstructInstance(DbProviderFactory instance)
         {
-            return (DbProviderFactory) (CreateInstance(type) ?? Activator.CreateInstance(type));
+            return CreateInstance(instance) ?? instance;
+        }
+
+        private static void EnsureIsInitialized()
+        {
+            if (!m_IsInitialized)
+            {
+                lock (m_Descriptions)
+                {
+                    if (!m_IsInitialized)
+                    {
+                        IncludeBuiltInFactoryClasses();
+                        IncludeFactoryClassesFromConfig();
+
+                        m_IsInitialized = true;
+                    }
+                }
+            }
+        }
+
+        private static void IncludeBuiltInFactoryClasses()
+        {
+            Set("system.data.sqlclient", SqlClientFactory.Instance);
+        }
+
+        private static void IncludeFactoryClassesFromConfig()
+        {
+            var section = ConfigurationManager.GetSection("system.data") as DataSet;
+
+            if (section == null
+                || !section.Tables.Contains("DbProviderFactories")
+                || !section.Tables["DbProviderFactories"].Columns.Contains("InvariantName")
+                || !section.Tables["DbProviderFactories"].Columns.Contains("AssemblyQualifiedName"))
+            {
+                return;
+            }
+
+            for (var i = 0; i < section.Tables["DbProviderFactories"].Rows.Count; i++)
+            {
+                var providerName = section.Tables["DbProviderFactories"].Rows[i]["InvariantName"] as string;
+                var providerType = section.Tables["DbProviderFactories"].Rows[i]["AssemblyQualifiedName"] as string;
+
+                DbProviderFactory instance = null;
+
+                var instanceType = Type.GetType(providerType);
+                var instanceField = instanceType.GetField("Instance", BindingFlags.DeclaredOnly | BindingFlags.Static | BindingFlags.Public);
+
+                if (instanceField != null
+                    && instanceField.FieldType.IsSubclassOf(typeof (DbProviderFactory)))
+                {
+                    try
+                    {
+                        instance = (DbProviderFactory) instanceField.GetValue(null);
+                    }
+                    catch
+                    {
+                        // ignored
+                    }
+                }
+
+                if (instance == null)
+                {
+                    try
+                    {
+                        instance = (DbProviderFactory) Activator.CreateInstance(instanceType);
+                    }
+                    catch
+                    {
+                        // ignored
+                    }
+                }
+
+                if (instance != null)
+                {
+                    Set(providerName, instance);
+                }
+            }
         }
     }
 }
